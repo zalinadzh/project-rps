fa25188bd2f66be63fbc66e552b0da6ab83d044c
zalinadzh <z.dzhambotova@mail.ru>
2026-02-24 19:03:42 +0300

    Finalize rock paper scissors: encapsulate logic in playGame function

--- script.js
+++ script.js
@@ -11,35 +11,41 @@ function getComputerChoice() {
 console.log(getComputerChoice());
 
 function getHumanChoice() {
- let choice = prompt("Введите ваш выбор: rock, paper или scissors");
- return choice;
+    let choice = prompt("Введите ваш выбор: rock, paper или scissors");
+    return choice;
 }
 console.log(getHumanChoice());
 
-let humanScore = 0;
-let computerScore = 0;
+function playGame() {
+    let humanScore = 0;
+    let computerScore = 0;
 
-function playRound(humanChoice, computerChoice) {
-    humanChoice = humanChoice.toLowerCase();
-    computerChoice.toLowerCase();
-    if (humanChoice === computerChoice) {
-        console.log("It's a tie! both chose " + humanChoice);
-    } else if (
-        (humanChoice === "rock" && computerChoice === "scissors") ||
-        (humanChoice === "paper" && computerChoice === "rock") ||
-        (humanChoice === "scissors" && computerChoice === "paper")
-    ) {
-        humanScore++;
-        console.log("You win! " + humanChoice + " beats " + computerChoice);
-    } else {
-        computerScore++;
-        console.log("You lose! " + computerChoice + " beats " + humanChoice);
+    function playRound(humanChoice, computerChoice) {
+        humanChoice = humanChoice.toLowerCase();
+        computerChoice.toLowerCase();
+        if (humanChoice === computerChoice) {
+            console.log("It's a tie! both chose " + humanChoice);
+        } else if (
+            (humanChoice === "rock" && computerChoice === "scissors") ||
+            (humanChoice === "paper" && computerChoice === "rock") ||
+            (humanChoice === "scissors" && computerChoice === "paper")
+        ) {
+            humanScore++;
+            console.log("You win! " + humanChoice + " beats " + computerChoice);
+        } else {
+            computerScore++;
+            console.log("You lose! " + computerChoice + " beats " + humanChoice);
+        }
     }
-}
-const humanSelection = getHumanChoice();
-const computerSelection = getComputerChoice();
 
-playRound(humanSelection, computerSelection);
+        for (let i = 0; i < 5; i++) {
+            const humanSelection = getHumanChoice();
+            const computerSelection = getComputerChoice();
+            playRound(humanSelection, computerSelection);
+        }
+        console.log("Final Score -> You: " + humanScore + " | Computer: " + computerScore);
+    }
+    playGame();
 
 
 
